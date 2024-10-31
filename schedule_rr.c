067a9298b2ef278184d2113524ff54344f4dee33
Mandeep Masoun <mandeepmasoun@yahoo.com>
2024-10-31 16:55:10 -0700

    found

--- schedule_rr.c
+++ schedule_rr.c
@@ -12,146 +12,119 @@
 
 #include "cpu.h"
 
-struct node *head = NULL;
-struct node *last = NULL;
-struct node *new = NULL;
+struct node *root = NULL;
+struct node *end = NULL;
+struct node *newNode = NULL;
 struct node *test = NULL;
 int num = 0;
 
 void add(char *name, int priority, int burst) {
   num = num + 1;
-  if (head == NULL) {
-    head = malloc(sizeof(struct node));
-    last = malloc(sizeof(struct node));
+  if (root == NULL) {
+    root = malloc(sizeof(struct node));
+    end = malloc(sizeof(struct node));
+    root->task = malloc(sizeof(struct task));
 
-    // set the name of the task
-    head->task = malloc(sizeof(struct task));
-    head->task->name = name;
-    head->task->burst = burst;
-    head->task->priority = priority;
-    // set the next node to be null
-    head->next = NULL;
+    root->task->burst = burst;
+    root->task->name = name;
+    root->task->priority = priority;
+    root->next = NULL;
 
-    last = head;
-  }
+    end = root;
+  } else {
 
-  else {
-
-    new = malloc(sizeof(struct node));
-    last->next = new;
-    new->task = malloc(sizeof(struct task));
-    new->task->name = name;
-    new->task->burst = burst;
-    new->task->priority = priority;
-    new->next = NULL;
-    last = new;
+    newNode = malloc(sizeof(struct node));
+    end->next = newNode;
+    newNode->task = malloc(sizeof(struct task));
+    newNode->task->name = name;
+    newNode->task->burst = burst;
+    newNode->task->priority = priority;
+    newNode->next = NULL;
+    end = newNode;
   }
 }
 
 // invoke the scheduler
 void schedule() {
 
-  struct node *current = head;
-  struct node *ref = head;
+  struct node *current = root;
+  struct node *ref = root;
   struct node *monitor = NULL;
   monitor = malloc(sizeof(struct node));
-  monitor = head;
-  int newburst = 0;
+  monitor = root;
+  int newNodeburst = 0;
   int totburst = 0;
   float turnaroundtime = 0;
   float ResponseTime = 0;
   float WaitTime = 0;
-  int check = 1;
+  
 
   while (ref != NULL) {
 
     if (ref->task->burst >= 10) {
 
-      newburst = ref->task->burst - 10; // set newburst to 10
+      newNodeburst = ref->task->burst - 10; // set newNodeburst to 10
 
-      if (current != head) {
+      if (current != root) {
         while (monitor != current) {
           if (strcmp(current->task->name, monitor->task->name) == 0) {
-            check = 0;
+
             break;
           }
           monitor = monitor->next;
         }
-        monitor = head;
-      }
-
-      if (ref->task->burst > 10 && ref != head) {
-        if (ref->next != NULL) {
-          if (check == 1) {
-            ResponseTime = ResponseTime + totburst;
-          }
-        }
-      }
-
-      if (ref->task->burst == 10) {
-
-        if (check == 1) {
-          ResponseTime = ResponseTime + totburst;
-        }
+        monitor = root;
       }
 
       totburst = totburst + 10; // totburst is 60
-      if (ref->task->burst == 10) {
-
-        turnaroundtime = totburst + turnaroundtime; // turnaroundtime = 60
-      }
-
       ref->task->burst = 10;
-
       run(ref->task, 10);
+      printf("        Time is now: %d\n", totburst);
     } else if (ref->task->burst < 10) {
-      newburst = 0;
-      if (check == 1) {
-        ResponseTime = ResponseTime + totburst;
-      }
+      newNodeburst = 0;
+
       totburst = totburst + ref->task->burst;
 
-      turnaroundtime = turnaroundtime + totburst;
-
       run(ref->task, ref->task->burst);
+      printf("        Time is now: %d\n", totburst);
     }
 
     while (1) {
       if (current->next != NULL) {
         current = current->next; // current = T4;
         if (current->next == NULL) {
-          if (newburst != 0) {
-            struct node *newnode = malloc(sizeof(struct node));
-            newnode->task = malloc(sizeof(struct task));
-            newnode->task->name = ref->task->name;
-            newnode->task->burst = newburst;
-            newnode->task->priority = ref->task->priority;
-            ref = ref->next;         // ref points to P2
-            current->next = newnode; // P4 points to New Node P1
-            newnode->next = NULL;    // P4 points to null
-            current = ref;           // current points to P2
+          if (newNodeburst != 0) {
+            struct node *newNodenode = malloc(sizeof(struct node));
+            newNodenode->task = malloc(sizeof(struct task));
+            newNodenode->task->name = ref->task->name;
+            newNodenode->task->burst = newNodeburst;
+            newNodenode->task->priority = ref->task->priority;
+            ref = ref->next;             // ref points to P2
+            current->next = newNodenode; // P4 points to newNode Node P1
+            newNodenode->next = NULL;    // P4 points to null
+            current = ref;               // current points to P2
 
             break;
           }
 
-          else if (newburst == 0) {
+          else if (newNodeburst == 0) {
             ref = ref->next; // ref points to T3
             current = ref;   // current points to T3
             break;
           }
         }
-      } else { // if this is the last element
+      } else { // if this is the end element
 
-        if (newburst != 0) {
-          struct node *new = malloc(sizeof(struct node));
-          new->task = malloc(sizeof(struct task));
-          new->task->name = ref->task->name;
-          new->task->priority = ref->task->priority;
-          new->task->burst = newburst;
-          ref->next = new;
+        if (newNodeburst != 0) {
+          struct node *newNode = malloc(sizeof(struct node));
+          newNode->task = malloc(sizeof(struct task));
+          newNode->task->name = ref->task->name;
+          newNode->task->priority = ref->task->priority;
+          newNode->task->burst = newNodeburst;
+          ref->next = newNode;
           ref = ref->next;
-          new->next = NULL;
-          current = new;
+          newNode->next = NULL;
+          current = newNode;
         } else {
           ref = ref->next;
         }
